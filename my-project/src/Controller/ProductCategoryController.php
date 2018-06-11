7ca128032ccaf03fa88735a769f28c1513f87c19
msobon <m.sobon@wearevirtua.com>
2018-06-11 09:17:46 +0200

    Added Bootstrap

--- my-project/src/Controller/ProductCategoryController.php
+++ my-project/src/Controller/ProductCategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\ProductCategory;
 use App\Form\ProductCategoryType;
 use App\Repository\ProductCategoryRepository;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,18 +33,26 @@ class ProductCategoryController extends Controller
         $date->format("Y:M:D");
         $productCategory->setDateOfCreation($date);
         $productCategory->setDateOfLastModification($date);
+
         $form = $this->createForm(ProductCategoryType::class, $productCategory);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($productCategory);
-            $em->flush();
+            try
+            {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($productCategory);
+                $em->flush();
 
-            $this->addFlash(
-                'notice',
-                'Your category was added'
-            );
+                $this->addFlash(
+                    'notice',
+                    'Your category was added'
+                );
+            }
+            catch(\Exception $exception)
+            {
+                $this->_addDatabaseErrorFlash();
+            }
 
             return $this->redirectToRoute('product_category_index');
         }
@@ -70,10 +79,19 @@ class ProductCategoryController extends Controller
         $form = $this->createForm(ProductCategoryType::class, $productCategory);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+
             $date = new \DateTime();
             $date->format("Y:M:D");
             $productCategory->setDateOfLastModification($date);
-            $this->getDoctrine()->getManager()->flush();
+
+            try
+            {
+                $this->getDoctrine()->getManager()->flush();
+            }
+            catch(\Exception $exception)
+            {
+               $this->_addDatabaseErrorFlash();
+            }
 
             $this->addFlash(
                 'notice',
@@ -95,22 +113,37 @@ class ProductCategoryController extends Controller
     public function delete(Request $request, ProductCategory $productCategory): Response
     {
         if ($this->isCsrfTokenValid('delete'.$productCategory->getId(), $request->request->get('_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($productCategory);
-            $em->flush();
-
-            $this->addFlash(
-                'notice',
-                'Your category was deleted'
-            );
+            try
+            {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($productCategory);
+                $em->flush();
+                $this->addFlash(
+                    'notice',
+                    'Your category was deleted'
+                );
+            }
+            catch(\Exception $exception)
+            {
+                $this->_addDatabaseErrorFlash();
+            }
         }
         else{
             $this->addFlash(
                 'error',
-                'Operation failed');
+                'Operation failed'
+            );
         }
 
         return $this->redirectToRoute('product_category_index');
+    }
+
+    private function _addDatabaseErrorFlash()
+    {
+        $this->addFlash(
+            'error',
+            'Problem with the database, please try later'
+        );
     }
 
 }
