c9420f6c67d19513a8785370a96d43be3c0c67b3
msobon <m.sobon@wearevirtua.com>
2018-06-25 13:57:04 +0200

    Removed useless code and add ignore uploads/csv

--- my-project/src/Controller/ProductCategoryApiController.php
+++ my-project/src/Controller/ProductCategoryApiController.php
@@ -20,13 +20,8 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Serializer\Encoder\CsvEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProductCategoryApiController extends Controller
